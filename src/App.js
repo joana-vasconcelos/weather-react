37c82e4088f422a994542a03d6ba1597d31107e9
joanajvc <j.v.carvalheiro@gmail.com>
2021-06-21 15:32:50 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Footer from "./Footer";
 import Weather from "./Weather";
-import Forecast from "./Forecast";
 
 export default function App() {
   return (
@@ -9,8 +8,6 @@ export default function App() {
       <div className="container">
         <div className="frame">
           <Weather defaultCity="Lisbon" />
-          <hr class="line" />
-          <Forecast />
         </div>
         <Footer />
       </div>
